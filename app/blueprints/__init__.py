6d3a1ec18fe5fdf8741990727c8d26e5b9d41251
Mikhail Profatsky <mixail.rostov04@gmail.com>
2023-01-12 21:36:43 +0300

    refactoring: deleted blueprints, old states and keyboards, renamed folder with videocards images

--- app/blueprints/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import admin_panel, games, income, main_menu, register, shop, support
-
-bps = [admin_panel.bp, games.bp, income.bp, main_menu.bp, register.bp, shop.bp, support.bp]
--- app/keyboards/admin_kb.py
+++
@@ -1,8 +0,0 @@
-from vkbottle.tools import Keyboard, KeyboardButtonColor, Text
-
-admin_keyboard = (
-    Keyboard(one_time=False, inline=False)
-    .add(Text('📃 Список обращений', payload={'admin': 'reports'}), color=KeyboardButtonColor.PRIMARY)
-    .add(Text('◀ В главное меню', payload={'main_menu': 'back'}))
-    .get_json()
-)
