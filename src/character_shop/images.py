379878709135b3d11c669e0c5121cc409a13cdcd
Mikhail Profatsky <mixail.rostov04@gmail.com>
2023-08-24 22:53:58 +0300

    updated shop page creation

--- src/character_shop/images.py
+++ src/character_shop/images.py
@@ -1,4 +1,4 @@
-from collections.abc import Iterable, Sequence
+from collections.abc import Iterable
 
 from PIL import ImageFont, ImageDraw, Image
 
@@ -9,28 +9,21 @@ from users.models_representations import Character
 def create_shop_image(
         characters: Iterable[Character],
         choice_numbers: Iterable[int],
-        prices: Sequence[int]
+        prices: Iterable[int]
 ) -> Image:
 
     choice_image = create_choice_image(characters, choice_numbers)
     font = ImageFont.truetype('assets/fonts/Fifaks10DEV1.ttf', size=40)
     draw_context = ImageDraw.Draw(choice_image)
-    draw_context.text(
-        (81 - 12 * len(str(prices[0])), 79),
-        f'${prices[0]:,}'.replace(',', '.'),
-        font=font,
-        fill='black'
-    )
-    draw_context.text(
-        (288 - 12 * len(str(prices[1])), 79),
-        f'${prices[1]:,}'.replace(',', '.'),
-        font=font,
-        fill='black'
-    )
-    draw_context.text(
-        (497 - 12 * len(str(prices[2])), 79),
-        f'${prices[2]:,}'.replace(',', '.'),
-        font=font,
-        fill='black'
-    )
+
+    x = 81
+    for price in prices:
+        draw_context.text(
+            (x - 12 * len(str(price)), 79),
+            f'${price:,}'.replace(',', '.'),
+            font=font,
+            fill='black'
+        )
+        x += 208
+
     return choice_image
